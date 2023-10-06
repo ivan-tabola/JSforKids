7cd7a5333e23fc14f3387acb09724aae3b975376
IvanTabola <j.tabola2016@yandex.by>
2023-10-06 16:22:16 +0300

    new

---
+++ 08.js
@@ -0,0 +1,13 @@
+var lemonChicken = false;
+var beefWithBlackBean = true;
+var sweetAndSourPork = true;
+
+if (lemonChicken) {
+    return "Отлично! Я буду курицу с лимоном!";
+} else if (beefWithBlackBean) {
+    return "Заказываю говядину.";
+} else if (sweetAndSourPork) {
+    return "Ладно, закажу свинину.";
+} else {
+    return "Что ж, отсается рис с яйцом.";
+}
