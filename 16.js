43d85b24a25b60578446ea017bc5dcd063f56661
IvanTabola <j.tabola2016@yandex.by>
2023-10-10 11:40:47 +0300

    стр.107

---
+++ 16.js
@@ -0,0 +1,12 @@
+//Задание на стр.107 для цикла for
+for (let i = 3; i < 10000; i = i * 3) {
+    console.log(i);
+}
+
+//тоже для цикла while
+let x = 3;
+while(x < 10000) {
+    console.log("Степени тройки: " + x + ";")
+    x = x * 3;
+}
+console.log(x);
