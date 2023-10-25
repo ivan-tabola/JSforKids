2d6e7b54a9098b302f8856e8fd68e9e49719d746
IvanTabola <j.tabola2016@yandex.by>
2023-10-25 23:40:04 +0300

    new

--- 20.js
+++ 20.js
@@ -2,7 +2,7 @@ var name1 = prompt("Как Вас зовут?");
 alert("Привет, " + name1);
 var likesCats = confirm("Тебе нравятся кошки?");
 if (likesCats) {
-    alert("Ты классная кошка?");
+    alert("Ты классная кошка!");
 } else {
     alert("Что ж, не проблема. Все равно ты молодец!");
 }
