4b1701caba7a5332bbe5d9c83510d625f9e7c42c
IvanTabola <j.tabola2016@yandex.by>
2023-10-25 23:39:12 +0300

    Виселица

---
+++ 20.js
@@ -0,0 +1,8 @@
+var name1 = prompt("Как Вас зовут?");
+alert("Привет, " + name1);
+var likesCats = confirm("Тебе нравятся кошки?");
+if (likesCats) {
+    alert("Ты классная кошка?");
+} else {
+    alert("Что ж, не проблема. Все равно ты молодец!");
+}
